8796890bc3ae0d1e76d3cc24c7433cf2ab10aff9
Jerry <jerry.guerrero95@gmail.com>
2022-04-15 23:47:05 -0500

    FirstAppearance for Hero class is string not Datetime

--- SuperheroStats/Data/Hero.cs
+++ SuperheroStats/Data/Hero.cs
@@ -39,7 +39,7 @@
         public string alterEgos { get; set; }
         public IList<string> aliases { get; set; }
         public string placeOfBirth { get; set; }
-        public DateTime firstAppearance { get; set; }
+        public string firstAppearance { get; set; }
         public string publisher { get; set; }
         public string alignment { get; set; }
 
