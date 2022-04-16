91a27122bc448e54f7326252621de9b71cf48366
Jerry <jerry.guerrero95@gmail.com>
2022-04-16 00:34:55 -0500

    hero service with API key secrets

--- SuperheroStats/Program.cs
+++ SuperheroStats/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using SuperheroStats.Data;
+using SuperheroStats.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +9,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
-
+builder.Services.AddHttpClient<IHeroService, HeroService>(client =>
+{
+    client.BaseAddress = new Uri("https://superhero-search.p.rapidapi.com/api/");
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
--- SuperheroStats/Services/HeroService.cs
+++ SuperheroStats/Services/HeroService.cs
@@ -5,10 +5,15 @@ namespace SuperheroStats.Services
 	public class HeroService : IHeroService
     {
         private readonly HttpClient _httpClient;
+        private readonly ILogger _logger;
+        private readonly IConfiguration _config;
 
-        public HeroService(HttpClient httpClient)
+        public HeroService(HttpClient httpClient, IConfiguration config)
         {
+            _config = config;
             _httpClient = httpClient;
+            _httpClient.DefaultRequestHeaders.Add("X-RapidAPI-Host", _config["HeroApiHost"]);
+            _httpClient.DefaultRequestHeaders.Add("X-RapidAPI-Key", _config["HeroApiKey"]);
         }
         public async Task<IEnumerable<Hero>> GetHeros()
         {
