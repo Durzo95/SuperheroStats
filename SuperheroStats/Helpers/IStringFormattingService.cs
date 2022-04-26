6780c44a258246bcc3afd9ace0ea9a52da202f7d
Jerry <jerry.guerrero95@gmail.com>
2022-04-26 01:25:00 -0500

    String formatting class

---
+++ SuperheroStats/Helpers/IStringFormattingService.cs
@@ -0,0 +1,8 @@
+﻿namespace SuperheroStats.Services;
+
+public interface IStringFormattingService
+{
+    object DeliminateStringIfList(object value);
+    string ToSentenceCase(string str);
+    public string ToTitleCase(string str);
+}
---
+++ SuperheroStats/Helpers/StringFormattingService.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace SuperheroStats.Services
+{
+    public class StringFormattingService : IStringFormattingService
+    {
+
+        private readonly TextInfo _ti = CultureInfo.CurrentCulture.TextInfo;
+
+        // This is for values in the Hero API that have both lists and strings
+        // For example, under Hero.Appearance there are some values that are strings and others are lists
+        // This function is needed in order to handle the list values
+        // This allows us to loop through the values in the razor page without writing if statements in each one to check for type
+        public object DeliminateStringIfList(object value)
+        {
+            // If value is not the Ilist string value type then just return the string value
+            if (value is not IList || !value.GetType().IsGenericType)
+                return value;
+            // Convert object to List<string> in order to use the string.Join method
+            return value is IList<string> valueListOfStrings ? string.Join(", ", valueListOfStrings) : "-";
+        }
+        // Converts pascal case strings into space deliminated string
+        // Used for the type names in the classes when looping
+        public string ToSentenceCase(string str)
+        {
+            str = Regex.Replace(str, "[a-z][A-Z]", m => $"{m.Value[0]} {char.ToLower(m.Value[1])}");
+            str = _ti.ToTitleCase(str);
+            return str;
+
+        }
+
+        public string ToTitleCase(string str)
+        {
+            return _ti.ToTitleCase(str);
+        }
+    }
+}
